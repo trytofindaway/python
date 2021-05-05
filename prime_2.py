ffb0b89951a432a0532ec7254633c30fe76f86f9
akiokanda <kanda7004@gmail.com>
2021-05-05 12:07:38 +0900

    aaaa

--- prime_2.py
+++ prime_2.py
@@ -1,34 +1,20 @@
-p = [True]*100
-p[0] = False
-p[1] = False
-n = 2
+def getSieveOfEratosthenes(n):
+    if not isinstance(n, int):
+        raise TypeError('n is int type.')
+    if n < 2:
+        raise ValueError('n is more than 2')
+    prime = [2]
+    limit = int(n ** 0.5)
+    data = [i + 1 for i in range(2, n, 2)]
+    while True:
+        p = data[0]
+        if limit <= p:
+            return prime + data
+        prime.append(p)
+        data = [e for e in data if e % p != 0]
 
 
-def chart():
-    s = ""
-    for i in range(100):
-        if p[i] == True:
-            s += "{:2d}, ".format(i)
-        else:
-            s += "／, "
-        if i % 10 == 9:
-            s += "\n"
-    print(s)
-
-
-def furui():
-    global n
-    for i in range(n + n, 100, n):
-        p[i] = False
-        print(n, "の倍数をふるい落としました")
-        chart()
-        while n < 100:
-            n = n + 1
-            if p[n] == True:
-                break
-
-
-chart()
-while n < 10:
-    input("Enterキーを押してください")
-    furui()
+if __name__ == '__main__':
+    data = getSieveOfEratosthenes(100)
+    print(' '.join(map(str, data)))
+    print("2~{0}までで以上が素数です".format(100))
